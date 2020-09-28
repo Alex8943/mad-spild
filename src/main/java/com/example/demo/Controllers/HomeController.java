6b6fdc5775708e2ecc3fa84c8eabb4a507b4c7fc
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2020-09-28 18:55:00 +0200

    opdated layout

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -8,6 +8,21 @@ public class HomeController {
 
     @GetMapping("/")
     public String frontPage(){
-        return "home/index";
+        return "home/forside/index";
+    }
+
+    @GetMapping("/fakta")
+    public String faktaFront(){
+        return "home/fakta/fakta-front";
+    }
+
+    @GetMapping("/omos")
+    public String omOsFront(){
+        return "home/om-os/om-os-front";
+    }
+
+    @GetMapping("/spare")
+    public String spareFront(){
+        return "home/spare/spare-front";
     }
 }
