771cc46215be9111764dd34396c13699099630fd
Blyatman666 <43138836+Blyatman666@users.noreply.github.com>
2020-09-28 21:45:57 +0200

    Har lavet funktionaliteten til min sparefunktion.

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -2,6 +2,9 @@ package com.example.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
@@ -24,5 +27,33 @@ public class HomeController {
     @GetMapping("/spare")
     public String spareFront(){
         return "home/spare/spare-front";
+    }
+    @PostMapping("/spare")
+    public String familie(@RequestParam short familiemedlemmer, Model model){
+        int familiebeløb = familiemedlemmer * 1800;
+        model.addAttribute("familiebel", "En gennemsnitlig familie på " + familiemedlemmer + " spilder mad for: " + familiebeløb + " kr. årligt");
+        return "home/spare/spare";
+    }
+    @PostMapping("/spare1")
+    public String alder(@RequestParam short alder, @RequestParam int dage, Model model1){
+        int alderbeløb = 0;
+        if(alder >= 16 && alder < 25){
+            alderbeløb = 29;
+        } else if(alder >= 25 && alder < 35){
+            alderbeløb = 33;
+        } else if(alder >= 35 && alder < 45){
+            alderbeløb = 28;
+        } else if(alder >= 45 && alder < 55){
+            alderbeløb = 29;
+        } else if(alder >= 55 && alder < 65){
+            alderbeløb = 37;
+        } else if(alder > 65){
+            alderbeløb = 27;
+        } else{
+            System.out.println("Information for this age is not available.");
+        }
+        alderbeløb = alderbeløb * dage;
+        model1.addAttribute("alderbel", "En person på " + alder + " år spilder i løbet af " + dage + " i gennemsnit mad for " + alderbeløb + " kr.");
+        return "home/spare/spare-alder";
     }
 }
